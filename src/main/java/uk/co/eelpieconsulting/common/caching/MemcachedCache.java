d1bb4d251b753f69cae199429a16dd9977ea7729
tony <tony@eelpieconsulting.co.uk>
2015-01-21 11:09:57 +0000

    Gracefully return cache misses in the event of a memcache fail.

--- src/main/java/uk/co/eelpieconsulting/common/caching/MemcachedCache.java
+++ src/main/java/uk/co/eelpieconsulting/common/caching/MemcachedCache.java
@@ -4,28 +4,47 @@ import java.io.IOException;
 
 import net.spy.memcached.AddrUtil;
 import net.spy.memcached.MemcachedClient;
+import net.spy.memcached.OperationTimeoutException;
+
+import org.apache.log4j.Logger;
 
 public class MemcachedCache implements Cache {
+	
+	private final static Logger log = Logger.getLogger(MemcachedCache.class);
 		
 	private final MemcachedClient memcachedClient;
 	
 	public MemcachedCache(String memcachedUrls) throws IOException {
-		this.memcachedClient= new MemcachedClient(AddrUtil.getAddresses(memcachedUrls));	// TODO this locks in stale DNS resolutions for the memcache hostnames		
+		this.memcachedClient= new MemcachedClient(AddrUtil.getAddresses(memcachedUrls));		
 	}
 	
 	@Override
 	public void put(String key, int ttl, Object value) {
-		memcachedClient.set(key, ttl, value);			
+		try {
+			memcachedClient.set(key, ttl, value);
+		} catch (OperationTimeoutException e) {
+			log.warn("Memcache timeout exception on put: " + e);
+		}
+			
 	}
 
 	@Override
 	public Object get(String key) {
-		return memcachedClient.get(key);		
+		try {
+			return memcachedClient.get(key);
+		} catch (OperationTimeoutException e) {
+			log.warn("Memcache timeout exception on get; returning null: " + e);
+			return null;
+		}
 	}
 	
 	@Override
 	public void decache(String key) {
-		memcachedClient.delete(key);			
+		try {
+			memcachedClient.delete(key);
+		} catch (OperationTimeoutException e) {
+			log.warn("Memcache timeout exception on delete: " + e);
+		}
 	}
 	
 }
